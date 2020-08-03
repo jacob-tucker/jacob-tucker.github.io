7c4cf325f92d893d1a61f25866411171b9e19862
BuildTools <unconfigured@null.spigotmc.org>
2020-08-03 15:55:58 -0400

    trying to fix navbar

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ const doIt = () => {
   var element2 = document.getElementById("mobile_selector");
   var element3 = document.getElementById("mobile_selector2");
 
-  for(let i = 0; i < 3; i++){
-    if(element.children[i].classList.contains("hello")){
+  for (let i = 0; i < 3; i++) {
+    if (element.children[i].classList.contains("hello")) {
       element.children[i].classList.remove("hello");
-    }else{
+    } else {
       element.children[i].classList.add("hello");
     }
   }
-  if(element.children[0].classList.contains("hello")){
+  if (element.children[0].classList.contains("hello")) {
     element2.style.visibility = 'visible';
     element2.classList.add('showIt');
     element3.style.visibility = 'visible';
     element3.classList.add('showIt');
-  }else{
+  } else {
     element2.style.visibility = 'hidden';
     element3.style.visibility = 'hidden';
   }
@@ -24,9 +24,9 @@ const doIt = () => {
 
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  if (prevScrollpos > currentScrollPos || window.scrollY === 0) {
     document.getElementById("navbar").style.top = "0";
     document.getElementById("bottominfo").style.top = '90%';
   } else {
